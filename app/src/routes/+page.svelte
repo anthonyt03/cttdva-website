d9ae6fbe9ae47cd3df68d10aa936eac902b44589
Anthony Tran <72509108+anthonyt03@users.noreply.github.com>
2024-10-13 23:38:24 -0400

    Added initial appbar

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+	import church from '$lib/assets/church.png';
+	import logo from '$lib/assets/logo.png';
+
+	function handleFaithDirect() {
+		window.open('https://membership.faithdirect.net/VA682', '_blank');
+	}
+	function handleHome() {
+		goto('http://localhost:5173/');
+	}
+</script>
+
+<title>Holy Martyrs of Vietnam Church</title>
+<AppBar
+	background="bg-success-900"
+	gridColumns="grid-cols-3"
+	slotDefault="place-self-center"
+	slotTrail="place-content-end"
+>
+	<svelte:fragment slot="lead">
+		<a href='http://localhost:5173/'>
+			<img class="w-32" alt="Church Logo" src={logo}/>
+		</a>
+	</svelte:fragment>
+	<p class="font-sans text-3xl">Holy Martyrs of Vietnam</p>
+	<svelte:fragment slot="trail">
+		<button type="button" class="btn variant-filled" on:click={handleFaithDirect}>
+			Faith Direct
+		</button>
+	</svelte:fragment>
+</AppBar>
